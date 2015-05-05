310e2c5a0809c24e7fda3e03fa8bba65166dfafd
BlueBlurXCIII <mn1amt@AMTC101-20.bolton.ac.uk>
2015-05-05 09:30:15 +0100

    AGT new
    current 05/05/2015

---
+++ Source/RollerBall/Private/SpeedPickup.cpp
@@ -0,0 +1,12 @@
+/*// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RollerBall.h"
+#include "SpeedPickup.h"
+
+
+ASpeedPickup::ASpeedPickup(const class FPostConstructInitializeProperties& PCIP)
+	: Super(PCIP)
+{
+	
+}*/
+
---
+++ Source/RollerBall/Public/SpeedPickup.h
@@ -0,0 +1,20 @@
+/*// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Pickup.h"
+#include "SpeedPickup.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROLLERBALL_API ASpeedPickup : public APickup
+{
+	GENERATED_UCLASS_BODY()
+
+	/** set the amount of speed increased*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Energy)
+	float EnergyLevel;
+
+};*/
